29e5551514d8f579e7eb36327131d19ece3423ca
Macyl <macyl.ahmed-zaid@etu.u-paris.fr>
2024-11-08 10:55:49 +0100

    feat: edit function to take a number as a paramater

--- src/99-bottles/my-solutions/99-bottles.spec.ts
+++ src/99-bottles/my-solutions/99-bottles.spec.ts
@@ -2,9 +2,19 @@ import { describe, expect, it } from 'vitest'
 import { getVerse } from './99-bottles'
 
 describe('99-bottles-song', () => {
-  it('should return the same given vers', () => {
-    const given = 'The verse to be returned'
-    const actual = getVerse(given)
-    expect(actual).toBe(given)
+  it('should return the same given verse', () => {
+    const expected = `99 bottles of beer on the wall, 99 bottles of beer.
+Take one down and pass it around, 98 bottles of beer on the wall.`
+
+    const actual = getVerse(99)
+    expect(actual).toBe(expected)
   })
+
+  //   it('should return the verse for 2 bottles', () => {
+  //     const expected = `1 bottle of beer on the wall, 1 bottle of beer.
+  // Take it down and pass it around, no more bottles of beer on the wall.`
+
+  //     const actual = getVerse(2)
+  //     expect(actual).toBe(expected)
+  //   })
 })
--- src/99-bottles/my-solutions/99-bottles.ts
+++ src/99-bottles/my-solutions/99-bottles.ts
@@ -1,3 +1,5 @@
-export function getVerse(verse: string) {
-  return verse
+export function getVerse(verse: number) {
+  const numberOfBottlesMinusOne = verse - 1
+  return `${verse} bottles of beer on the wall, ${verse} bottles of beer.
+Take one down and pass it around, ${numberOfBottlesMinusOne} bottles of beer on the wall.`
 }
