84fa231126f8b4a6f483756d8aa3e70fa5d65672
Macyl <Macyl23@github.com>
2025-02-13 17:24:52 +0100

    feat: implement getVerse function and tests to handle 4 cases

--- src/99-bottles/my-solutions/99-bottles.spec.ts
+++ src/99-bottles/my-solutions/99-bottles.spec.ts
@@ -1,20 +1,42 @@
-import { describe, expect, it } from 'vitest'
-import { getVerse } from './99-bottles'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { BottleSong } from './99-bottles'
 
 describe('99-bottles-song', () => {
+  let song: BottleSong
+
+  beforeEach(() => {
+    song = new BottleSong()
+  })
+
   it('should return the same given verse', () => {
     const expected = `99 bottles of beer on the wall, 99 bottles of beer.
 Take one down and pass it around, 98 bottles of beer on the wall.`
 
-    const actual = getVerse(99)
+    const actual = song.getVerse(99)
     expect(actual).toBe(expected)
   })
 
-  //   it('should return the verse for 2 bottles', () => {
-  //     const expected = `1 bottle of beer on the wall, 1 bottle of beer.
-  // Take it down and pass it around, no more bottles of beer on the wall.`
+  it('should return the verse for 2 bottles', () => {
+    const expected = `2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall.`
 
-  //     const actual = getVerse(2)
-  //     expect(actual).toBe(expected)
-  //   })
+    const actual = song.getVerse(2)
+    expect(actual).toBe(expected)
+  })
+
+  it('should return the verse for 1 bottle', () => {
+    const expected = `1 bottle of beer on the wall, 1 bottle of beer.
+Take it down and pass it around, no more bottles of beer on the wall.`
+
+    const actual = song.getVerse(1)
+    expect(actual).toBe(expected)
+  })
+
+  it('should return the verse for 1 bottle', () => {
+    const expected = `No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.`
+
+    const actual = song.getVerse(0)
+    expect(actual).toBe(expected)
+  })
 })
--- src/99-bottles/my-solutions/99-bottles.ts
+++ src/99-bottles/my-solutions/99-bottles.ts
@@ -1,5 +1,18 @@
-export function getVerse(verse: number) {
-  const numberOfBottlesMinusOne = verse - 1
-  return `${verse} bottles of beer on the wall, ${verse} bottles of beer.
+export class BottleSong {
+  getVerse(verse: number) {
+    const numberOfBottlesMinusOne = verse - 1
+    if (verse > 2) {
+      return `${verse} bottles of beer on the wall, ${verse} bottles of beer.
 Take one down and pass it around, ${numberOfBottlesMinusOne} bottles of beer on the wall.`
+    } else if (verse == 2) {
+      return `2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, ${numberOfBottlesMinusOne} bottle of beer on the wall.`
+    } else if (verse == 1) {
+      return `${verse} bottle of beer on the wall, ${verse} bottle of beer.
+Take it down and pass it around, no more bottles of beer on the wall.`
+    } else {
+      return `No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.`
+    }
+  }
 }
