6f59948b1754ff93f775208963ef0540dae62678
Macyl <Macyl23@github.com>
2025-03-25 13:58:12 +0100

    test: handle case of high and minus letter

--- src/caesar/my-solutions/caesarCipher.spec.ts
+++ src/caesar/my-solutions/caesarCipher.spec.ts
@@ -8,4 +8,14 @@ describe('Caesar Cipher', () => {
   test('another one, given one letter, should return the letter plus the shift', () => {
     expect(caesarCipher('a', 2)).toBe('c')
   })
+  test('given a character should return the same text', () => {
+    expect(caesarCipher('!', 2)).toBe('!')
+  })
+  test('given a an upper case letter should return upper case letter', () => {
+    expect(caesarCipher('A', 1)).toBe('B')
+  })
+  //   test('given a sequence, with a letter and a character should return the ciphered text with character', () => {
+  //     const sentence = 'I!'
+  //     expect(caesarCipher(sentence, 1)).toBe('J!')
+  //   })
 })
--- src/caesar/my-solutions/causarCiper.service.ts
+++ src/caesar/my-solutions/causarCiper.service.ts
@@ -1,5 +1,15 @@
 export function caesarCipher(str: string, shift: number) {
-  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
-  const cipheredText = alphabet.slice(str.indexOf(str[0]), shift + 1)
+  const MINUS_ALPHABET = 'abcdefghijklmnopqrstuvwxyz'.split('')
+  const HIGH_ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+
+  if (!MINUS_ALPHABET.includes(str[0].toLowerCase())) {
+    return str
+  }
+
+  const cipheredText =
+    str[0] === str[0].toLowerCase()
+      ? MINUS_ALPHABET.slice(MINUS_ALPHABET.indexOf(str[0]), shift + 1)
+      : HIGH_ALPHABET.slice(HIGH_ALPHABET.indexOf(str[0]), shift + 1)
+
   return cipheredText[cipheredText.length - 1]
 }
